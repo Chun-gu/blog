e3508e754555debde72572cd6f91e76affdb2e08
Chun-gu <chungu.yi92@gmail.com>
2023-07-15 03:04:58 +0900

    feat: add fetcher function for individual post

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,5 +1,26 @@
 type FileTree = { tree: [{ path: string }] }
 
+export async function getPostByFileName(
+  fileName: string
+): Promise<BlogPost | undefined> {
+  const res = await fetch(
+    `https://raw.githubusercontent.com/Chun-gu/blog-posts/main/${fileName}`,
+    {
+      headers: {
+        Accept: 'application/vnd.github+json',
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    }
+  )
+
+  if (!res.ok) return undefined
+
+  const rawMDX = await res.text()
+
+  if (rawMDX === '404: Not Found') return undefined
+}
+
 export async function getPostMeta(): Promise<Meta[] | undefined> {
   const res = await fetch(
     'https://api.github.com/repos/Chun-gu/blog-posts/git/trees/main?recursive=1',
@@ -16,7 +37,21 @@ export async function getPostMeta(): Promise<Meta[] | undefined> {
 
   const fileTree: FileTree = await res.json()
 
-  const filePaths = fileTree.tree
+  const fileNames = fileTree.tree
     .map((file) => file.path)
     .filter((path) => path.endsWith('.mdx'))
+
+  // const posts = await Promise.all(
+  //   fileNames.map(async (fileName) => {
+  //     const post = await getPostByFileName(fileName)
+  //     return post?.meta
+  //   })
+  // )
+
+  const posts = []
+
+  for (const fileName of fileNames) {
+    const post = await getPostByFileName(fileName)
+    if (post) posts.push(post.meta)
+  }
 }
